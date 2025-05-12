b70d1421ac521fa577333b85f6f674c62def5320
Ana Beatriz Zinatto Sobral <ana.sobral@sptech.school>
2025-05-12 14:24:24 -0300

    Atualizando a modelagem, adicionando a api data viz

--- Banco de Dados/Script-Projeto-Individual.sql
+++ Banco de Dados/Script-Projeto-Individual.sql
@@ -2,44 +2,42 @@ create database DogsWorld;
 
 use DogsWorld;
 
+
+create table sorteio (
+idSorteio int primary key auto_increment,
+bola int,
+tapete int,
+cama int,
+racao int,
+osso int,
+pote int
+);
+
+create table formulario (
+idVotacao int primary key auto_increment,
+shitzu int,
+golden int,
+viralata int,
+yorkshire int,
+pastorAlemao int,
+outro int,
+naoTem int,
+fkSorteio int,
+	constraint fkFormularioSorteio foreign key (fkSorteio) references sorteio (idSorteio)
+);
+
+
 create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar (45),
 email varchar (45),
-endereco varchar (200)
+endereco varchar (100),
+fkFormulario int,
+	constraint fkFormularioUsuario foreign key (fkFormulario) references formulario (idVotacao)
+	
 );
 
-create table votação (
-idVotacao int auto_increment,
-golden int,
-viralata int,
-shitzu int,
-yorkshire int,
-labrador int,
-outro int,
-nãotem int,
-fkUsuario int,
-	constraint fkUsuarioVotacao
-    foreign key (fkUsuario) references usuario (idUsuario),
-    primary key (idVotacao, fkUsuario)
-);
-
-create table sorteio (
-idSorteio int auto_increment,
-bola int,
-tapeteHigienico int, 
-cama int,
-racao int,
-coleira int,
-osso int, 
-pote int,
-fkVotacao int,
-fkUsuario int,
-	constraint fkVotacaoSorteio foreign key (fkVotacao) references votação (idVotacao),
-	constraint fkUsuarioSorteio foreign key (fkUsuario) references usuario (idUsuario),
-    primary key (idSorteio, fkVotacao, fkUsuario) 
-);
 
 select * from usuario;
-select * from votação;
+select * from formulario;
 select * from sorteio;
